30e2a51bb470c0aa9c75b862739a5c8c8067b8b6
julianapedroso <juliana.cristina@hotmail.com.br>
2022-01-26 01:38:14 -0300

    :sparkles: api connection working

--- client/src/components/TableProviders/index.js
+++ client/src/components/TableProviders/index.js
@@ -1,82 +1,46 @@
 import React from 'react';
 import './styles.scss';
+import PropTypes from 'prop-types';
 
-const TableProviders = ({
-  logo,
-  name,
-  homeState,
-  costKwh,
-  minKwhLimit,
-  totalNumberCustomers,
-  averageCustomerRating,
-}) => {
+const TableProviders = (props) => {
+  const {
+    logo,
+    name,
+    homeState,
+    costKwh,
+    minKwhLimit,
+    totalNumberCustomers,
+    averageCustomerRating,
+  } = props;
   return (
     <main className="TableProviders">
-      <table>
-        <tr className="table__title-header">
-          <th className="table__header">Logo</th>
-          <th className="table__header">Companhia</th>
-          <th className="table__header">Estado</th>
-          <th className="table__header">Custo kWh</th>
-          <th className="table__header">Limite mínimo kWh</th>
-          <th className="table__header">Nº Clientes</th>
-          <th className="table__header">Avaliações</th>
-          <th className="table__header">Ações</th>
-        </tr>
-        <tr className="table__content-row">
-          <td className="table__row row__image">
-            <img
-              src="https://seeklogo.com/images/L/Light_Logomarca-logo-0C4DF9D65C-seeklogo.com.png"
-              alt=""
-            />
-          </td>
-          <td className="table__row">LIGHT</td>
-          <td className="table__row">SP</td>
-          <td className="table__row">R$ 0.89</td>
-          <td className="table__row">30</td>
-          <td className="table__row">1.000.000</td>
-          <td className="table__row">7/10</td>
-          <td className="table__row">
-            <button className="btn__choose">Escolher</button>
-          </td>
-        </tr>
-        <tr className="table__content-row">
-          <td className="table__row row__image">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/EDP_logo.svg/1200px-EDP_logo.svg.png"
-              alt=""
-            />
-          </td>
-          <td className="table__row">EDP BANDEIRANTE</td>
-          <td className="table__row">SP</td>
-          <td className="table__row">R$ 0.92</td>
-          <td className="table__row">35</td>
-          <td className="table__row">689.000</td>
-          <td className="table__row">9/10</td>
-          <td className="table__row">
-            <button className="btn__choose">Escolher</button>
-          </td>
-        </tr>
-        <tr className="table__content-row">
-          <td className="table__row row__image">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Enel_Group_logo.svg/1024px-Enel_Group_logo.svg.png"
-              alt=""
-            />
-          </td>
-          <td className="table__row">ENEL</td>
-          <td className="table__row">RJ</td>
-          <td className="table__row">R$ 0.83</td>
-          <td className="table__row">30</td>
-          <td className="table__row">129.750</td>
-          <td className="table__row">4/10</td>
-          <td className="table__row">
-            <button className="btn__choose">Escolher</button>
-          </td>
-        </tr>
-      </table>
+      <tr className="table__content-row">
+        <td>
+          <img className="table__content-logo" src={logo} alt="Provider logo" />
+        </td>
+        <td className="table__row">{name}</td>
+        <td className="table__row">{homeState}</td>
+        <td className="table__row">R$ {costKwh}</td>
+        <td className="table__row">{minKwhLimit}</td>
+        <td className="table__row">{totalNumberCustomers}</td>
+        <td className="table__row">{averageCustomerRating}/10</td>
+        <td className="table__row">
+          <button className="btn__choose">Escolher</button>
+        </td>
+      </tr>
     </main>
   );
+};
+
+TableProviders.propTypes = {
+  id: PropTypes.string.isRequired,
+  logo: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  homeState: PropTypes.string.isRequired,
+  costKwh: PropTypes.number.isRequired,
+  minKwhLimit: PropTypes.number.isRequired,
+  totalNumberCustomers: PropTypes.number.isRequired,
+  averageCustomerRating: PropTypes.number.isRequired,
 };
 
 export default TableProviders;
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.scss';
+// API
+import api from '../../services/api';
 // Component
 import { TableProviders } from '../../components';
 
 const Home = () => {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    getAllProviders();
+  }, []);
+
+  const getAllProviders = async () => {
+    try {
+      const response = await api.get('/providers');
+      setProviders(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <main className="Home">
       <h1 className="home__title">
@@ -26,7 +43,49 @@ const Home = () => {
       <section className="home__providers">
         <h2 className="home__providers-title">Fornecedores</h2>
         <p>Gerencie suas fornecedores</p>
-        <TableProviders />
+        <article className="table__providers">
+          <table>
+            <thead>
+              <tr className="table__title-header">
+                <th className="table__header">Logo</th>
+                <th className="table__header company">Companhia</th>
+                <th className="table__header state">Estado</th>
+                <th className="table__header cost">Custo kWh</th>
+                <th className="table__header min-limit">Limite min kWh</th>
+                <th className="table__header customers">Nº Clientes</th>
+                <th className="table__header ratings">Avaliações</th>
+                <th className="table__header actions">Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              {providers.map((provider) => {
+                const {
+                  _id,
+                  logo,
+                  name,
+                  homeState,
+                  costKwh,
+                  minKwhLimit,
+                  totalNumberCustomers,
+                  averageCustomerRating,
+                } = provider;
+                return (
+                  <div key={_id}>
+                    <TableProviders
+                      logo={logo}
+                      name={name}
+                      homeState={homeState}
+                      costKwh={costKwh}
+                      minKwhLimit={minKwhLimit}
+                      totalNumberCustomers={totalNumberCustomers}
+                      averageCustomerRating={averageCustomerRating}
+                    />
+                  </div>
+                );
+              })}
+            </tbody>
+          </table>
+        </article>
       </section>
     </main>
   );
