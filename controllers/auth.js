f509c7ccd9aab3fe05c0f0a5eab876e3ed573262
julianapedroso <juliana.cristina@hotmail.com.br>
2022-01-27 23:18:49 -0300

    :lock: security token working

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
 const Client = require('../models/Client');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-exports.signup = async (req, res, next) => {
+exports.signup = async (req, res) => {
   const { name, email, password } = req.body;
 
   // Create password hash
@@ -28,25 +29,67 @@ exports.signup = async (req, res, next) => {
   }
 };
 
-exports.signin = async (req, res, next) => {
-  // const { email, password } = req.body;
-  // if (!email || !password) {
-  //   res.status(400).json({
-  //     success: false,
-  //     error: 'Please provide email and password',
-  //   });
-  //   return;
-  // }
-  // try {
-  //   const client = await Client.findOne({ email }).select('+password');
-  //   if (!client) {
-  //     res.status(404).json({ success: false, error: 'Invalid credentials' });
-  //     return;
-  //   }
-  // } catch (error) {
-  //   res.status(500).json({
-  //     success: false,
-  //     error: error.message,
-  //   });
-  // }
+exports.signin = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).json({
+      success: false,
+      error: 'Please provide email and password',
+    });
+    return;
+  }
+  try {
+    const client = await Client.findOne({ email: email });
+    if (!client) {
+      res.status(404).json({ success: false, error: 'User not found' });
+      return;
+    }
+
+    // check if password match
+    const checkPassword = await bcrypt.compare(password, client.password);
+
+    if (!checkPassword) {
+      res.status(404).json({ success: false, error: 'Invalid credentials' });
+      return;
+    }
+
+    try {
+      const secret = process.env.SECRET;
+      const token = jwt.sign(
+        {
+          id: client._id,
+        },
+        secret
+      );
+
+      res.status(200).json({
+        success: true,
+        mssage: 'Authentication performed successfully!',
+        token,
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        error: error.message,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
+
+// READ BY ID 
+exports.client = async (req, res) => {
+  const id = req.params.id
+  const client = await Client.findById(id, '-password');
+
+  if (!client) {
+    res.status(404).json({ success: false, error: 'User not found' });
+    return;
+  }
+
+  res.status(200).json(client);
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { signup, signin } = require('../controllers/auth');
+const { signup, signin, client } = require('../controllers/auth');
 
 router.route('/signup').post(signup);
 router.route('/signin').post(signin);
+router.route('/:id').get(client);
 
 module.exports = router;
