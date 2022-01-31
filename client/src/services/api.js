e10f7e3970162eb525836305d96284e73350a95b
julianapedroso <juliana.cristina@hotmail.com.br>
2022-01-31 10:42:03 -0300

    :hammer: api.js refactored

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 import { BASE_URL } from './constants';
 
-const axiosInstance = axios.create({
+export const api = axios.create({
   baseURL: BASE_URL,
 });
 
-export default axiosInstance;
+export const createSession = async (email, password) => {
+  return api.post('/auth/signin', { email, password });
+};
+
+export const getProviders = async () => {
+  return api.get('/providers');
+};
